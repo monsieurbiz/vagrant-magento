b6d21095b03a4aaee53f170e5a393a51b37414d8
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2014-02-23 19:23:27 +0100

    Added tmux (+config file)
    The bind-key is: C-z
    To split verticaly: bind-key |
    To split horizontaly: bind-key -
    
    You can use the mouse :')

--- puppet/modules/server/manifests/init.pp
+++ puppet/modules/server/manifests/init.pp
@@ -17,7 +17,7 @@ class server ( $hostname ) {
     }
 
     # Few packages
-    $packages = ["curl", "tidy", "screen", "vim", "htop"]
+    $packages = ["curl", "tidy", "screen", "vim", "htop", "tmux"]
     package { $packages:
         ensure  => latest,
         require => Exec['update'],
@@ -33,6 +33,18 @@ class server ( $hostname ) {
         ensure  => file,
         path    => '/home/vagrant/.screenrc',
         source  => 'puppet:///modules/server/.screenrc',
+    }
+
+    # Tmux config
+    file { ".tmux.conf":
+        ensure  => file,
+        path    => '/root/.tmux.conf',
+        source  => 'puppet:///modules/server/.tmux.conf',
+    }
+    file { ".tmux.conf for vagrant":
+        ensure  => file,
+        path    => '/home/vagrant/.tmux.conf',
+        source  => 'puppet:///modules/server/.tmux.conf',
     }
 
 }
